45f4d957a9e46cabfe59b5f509bf76aeed8bb287
lohgdds1 <lorenaguedes2015lg@gmail.com>
2025-03-10 17:51:35 -0300

    Springfield

---
+++ src/main/java/com/springfield/cidade_springfield/CidadeSpringfieldApplication.java
@@ -0,0 +1,13 @@
+package com.springfield.cidade_springfield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadeSpringfieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadeSpringfieldApplication.class, args);
+	}
+
+}
